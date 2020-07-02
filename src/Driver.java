a2e10720a029650dadb099c372f9c076d45aa133
Jose Lara <jose.lara@adidas.com>
2020-07-03 01:43:05 +0200

    This is the initial commit	from Eclipse

---
+++ src/Driver.java
@@ -0,0 +1,40 @@
+import org.apache.commons.lang3.time.StopWatch;
+
+/**
+ * 
+ */
+
+/**
+ * @author larajos
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Running...");
+		
+		StopWatch myStopWatch = new StopWatch();
+		myStopWatch.start();
+		
+		performLenghtyProcess();	
+		
+		myStopWatch.stop();
+		
+		System.out.println("Finished....");
+		System.out.println("Time Processing was: " + myStopWatch.getTime());
+	}
+	
+	private static void performLenghtyProcess() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+}
